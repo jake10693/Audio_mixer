f0755075e11295b2d62fc1b4c484f2b2e4dc5115
Jacob Carver <jcarver@intellaegis.com>
2021-03-24 20:30:02 -0700

    first commit

---
+++ AppDescription/FreeflowIdeas.js
@@ -0,0 +1,7 @@
+const bookSongDjMixer = {
+    PrimaryConcept: "can mix and adjust sound settings from the audio file of a book with the audiofile of a song like a dj booth",
+    ConvenienceFactors: {
+        Factor1: 'Can play multiple sound files at once while being able to adjust the settings for each track playing volume ',
+    }
+
+}
---
+++ Assets/script.js
@@ -0,0 +1,5 @@
+// const trackVolumeSlider = document.querySelector('.slider');
+// const trackVolume = trackVolumeSlider.value();
+// let sound = document.getElementById('trackAudio');
+// sound.volume = 0.9
+
